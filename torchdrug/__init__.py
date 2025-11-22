493c850e6e75bdfef78d600d10e7a12a09cfdcd8
HoangBach <adamhermes66@gmail.com>
2025-11-22 20:42:50 +0700

    fix torchdrug

--- torchdrug/__init__.py
+++ torchdrug/__init__.py
@@ -1,5 +1,5 @@
 # Minimal top-level package to mimic torchdrug structure used by the repo.
 from . import core, data, layers, utils
-from .core import Registry as R
+from .core import R
 
 __all__ = ["core", "data", "layers", "utils", "R"]
