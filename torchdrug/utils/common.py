9e80c03b3dea5f0e066cc06eb3f16d60e284e71c
HoangBach <adamhermes66@gmail.com>
2025-11-22 20:36:37 +0700

    fix torchdrug

--- torchdrug/utils/common.py
+++ torchdrug/utils/common.py
@@ -8,3 +8,22 @@ def sparse_coo_tensor(indices, values, size, device=None, dtype=None):
         indices = indices.to(device)
         values = values.to(device)
     return torch.sparse_coo_tensor(indices, values, size).coalesce()
+
+class cached_property(object):
+    """
+    Simple cached_property implementation compatible with TorchDrug.
+    Usage:
+        @cached_property
+        def x(self): ...
+    """
+    def __init__(self, func):
+        self.func = func
+        self.__doc__ = getattr(func, "__doc__")
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        value = self.func(instance)
+        setattr(instance, self.func.__name__, value)
+        return value
+
