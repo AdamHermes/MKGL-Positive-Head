f6543edea5f05997cbf1626f6755713fdc8eb103
HoangBach <adamhermes66@gmail.com>
2025-11-23 08:00:39 +0700

    fix torchdrug data

--- torchdrug/datasets/fb15k.py
+++ torchdrug/datasets/fb15k.py
@@ -2,8 +2,9 @@ import os
 
 from torch.utils import data as torch_data
 
-from . import data, utils
-from ..core import Registry as R
+from .. import data, utils
+from ..core import R
+
 
 
 @R.register("datasets.FB15k")
